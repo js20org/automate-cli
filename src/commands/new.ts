53425c6e41f11f18a1c761fd52da89de27e8be3e
Robin Sveningson <robinsveningson@gmail.com>
2022-10-29 11:52:05 +0200

    Made sure new command works in production

--- src/commands/new.ts
+++ src/commands/new.ts
@@ -1,20 +1,40 @@
 import path from 'path';
 
-import { ILogger } from '../types';
-
 import {
     CommandExecutorService,
     FilesystemService,
     runGenerator,
 } from 'rjsv-templates';
 
-export const runNew = async (_logger: ILogger) => {
-    const workingDirectory = process.cwd();
-    const filesRootPath = path.resolve(
+import { hasFile } from '../utils';
+import { ILogger } from '../types';
+
+const getRootPath = () => {
+    const devPath = path.resolve(
         __dirname,
         '..',
         'node_modules/rjsv-templates/files'
     );
+
+    const hasDev = hasFile(devPath);
+
+    if (hasDev) {
+        return devPath;
+    }
+
+    const prodPath = path.resolve(__dirname, '../..', 'rjsv-templates/files');
+    const hasProd = hasFile(prodPath);
+
+    if (hasProd) {
+        return prodPath;
+    }
+
+    throw new Error('Failed to find rjsv-templates files.');
+};
+
+export const runNew = async (_logger: ILogger) => {
+    const workingDirectory = process.cwd();
+    const filesRootPath = getRootPath();
 
     await runGenerator(
         filesRootPath,
